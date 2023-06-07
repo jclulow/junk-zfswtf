a1d56ee7b143ce8e30ae0285fc6f1ff691312f6f
Joshua M. Clulow <jmc@oxide.computer>
2023-06-07 00:28:27 -0700

    fix comments to reflect a more complete understanding

--- src/main.rs
+++ src/main.rs
@@ -139,6 +139,27 @@ fn main() -> Result<()> {
 
     for arg in a.free.iter() {
         let path = CString::new(arg.clone())?;
+        println!("looking at path: {path:?}");
+
+        /*
+         * Perform a statvfs(2) call against the path.  Though we are providing
+         * a path that is potentially not the mount point of any particular file
+         * system, the call will determine which file system the file resides
+         * in.
+         *
+         * Even though we have nominated a file, this call is specific to the
+         * file system.  The process of automatically mounting snapshots under
+         * ".zfs/snapshot/NAME/..." is somewhat magical and does not result in a
+         * visible mount entry for the snapshot; see zfsctl_snapdir_lookup().
+         * Some of this magic is in service of NFS exports of ZFS file systems,
+         * to make the snapshot appear as effectively just a regular directory
+         * and thus not require an explicit and separate NFS mount on the client
+         * to cross into the snapdir.  Nonetheless, this also serves our
+         * purposes here.
+         *
+         * This call will end up telling us details about the live file system,
+         * even if we hit a snapshot.
+         */
         let mut f: statvfs = unsafe { std::mem::zeroed() };
         let res = unsafe { statvfs(path.as_ptr(), &mut f) };
         if res != 0 {
@@ -153,61 +174,57 @@ fn main() -> Result<()> {
         println!("f_basetype = {basetype:?}");
 
         /*
-         * Curiously, and possibly as an accident of history, ZFS snapshots are
-         * not shown as read-only in file system flags returned by statvfs!  One
-         * could argue that this is a bug which we should go and fix.
-         */
-        if (f.f_flag & libc::ST_RDONLY) != 0 {
-            println!("read only!");
-        } else {
-            println!("NOT read only!");
-        }
-
-        /*
          * The fsid value is, today, the 32-bit compressed version of the unique
-         * device ID that ZFS created for the file system or snapshot in
-         * question.  These IDs are ephemeral for the current import of the ZFS
-         * pool in question, but can be used to distinguish one dataset or
-         * snapshot from another.
+         * device ID that ZFS created for the file system in question.  These
+         * IDs are ephemeral for the current import of the ZFS pool in question,
+         * but can be used to distinguish one dataset or snapshot from another.
          *
-         * Curiously, in the snapshot case, the statvfs(2) f_fsid value reflects
-         * the unique device number of the _file system_ rather than the
-         * snapshot itself.  Unfortunately because it is a compressed device ID,
-         * and this is a 64-bit system, I do not believe there is any public
-         * function that allows us to expand back to a native width device ID.
-         * Fortunately, the "dev" mount option is _also_ a compressed device, so
+         * Unfortunately because it is a compressed device ID and this is a
+         * 64-bit system, I do not believe there is any public function that
+         * allows us to expand back to a native width device ID.  Fortunately,
+         * the "dev" mount option is _also_ expressed as a compressed device, so
          * for now we'll just compare that string to this number.
          */
-        println!("f_fsid -> {:x}", f.f_fsid);
+        println!("f_fsid = 0x{:x}", f.f_fsid);
 
+        /*
+         * Now, make a stat(2) call against the path.  This call _is_
+         * vnode-specific, and thus some of the information we get will be
+         * lifted from the snapshot if this is one.
+         */
         let mut st: stat = unsafe { std::mem::zeroed() };
         let res = unsafe { stat(path.as_ptr(), &mut st) };
         if res != 0 {
             bail!("stat({:?}) failed: {}", a.free[0], last_errno());
         }
 
-        println!("st_dev = {:x}", st.st_dev);
+        /*
+         * The device number is, again, ephemeral to this import but unique on
+         * the system at any given moment.  Snapshots get their own device
+         * numbers, which are visible through stat(2).  See
+         * zfs_create_unique_device().
+         */
+        println!("st_dev = 0x{:x}", st.st_dev);
 
         let fs_major = unsafe { major(st.st_dev) };
         let fs_minor = unsafe { minor(st.st_dev) };
-        println!("st_dev fs_major -> {fs_major}");
-        println!("st_dev fs_minor -> {fs_minor}");
+        println!("st_dev major() -> {fs_major} (0x{fs_major:x})");
+        println!("st_dev minor() -> {fs_minor} (0x{fs_minor:x})");
 
         /*
-         * Get the st_fstype string ready for comparison and confirm
-         * it matches what we got from statvfs(2).
+         * Get the st_fstype string ready for comparison and confirm it matches
+         * what we got from statvfs(2).
          *
-         * The libc crate "struct stat" definition has made a curious
-         * decision to make "st_fstype" into a _private_ field named
-         * __unused.  It's hard to understand why such a hostile decision
-         * has arisen, but in the mean time it is still our computer, so:
+         * The libc crate has made a curious decision to make "st_fstype" into a
+         * _private_ field named __unused.  It's hard to understand why such a
+         * hostile situation has arisen, but in the mean time it is in fact our
+         * computer:
          */
         let fstypeaddr =
             ((std::ptr::addr_of!(st) as usize) + 0x70) as *const c_char;
 
         let st_fstype = unsafe { CStr::from_ptr(fstypeaddr) };
         let st_fstype = st_fstype.to_str().unwrap();
-        println!("st_fstype = {st_fstype:?}");
 
         if st_fstype != basetype {
             bail!("st_fstype {st_fstype:?} != f_basetype {basetype:?}");
@@ -237,6 +254,9 @@ fn main() -> Result<()> {
 
             /*
              * Does this mount entry match the file system device ID we got?
+             * This ID is for the live file system, whether or not we were
+             * looking at a snapshot, so it must match something visible in the
+             * mount table.
              */
             if ent.getopt("dev") != Some(format!("{:x}", f.f_fsid)) {
                 return None;
@@ -244,14 +264,15 @@ fn main() -> Result<()> {
 
             if fs_minor != ent.minor {
                 /*
-                 * If the file system device minor number does not match the
-                 * mount entry, but the fsid does, this is a snapshot of that
-                 * file system.
+                 * If the file system device minor number from stat(2) does not
+                 * match the mount entry, but the statvfs(2) fsid does, this is
+                 * a snapshot of that file system.
                  */
                 Some(WhatIsIt::Snapshot(ent.special.to_string()))
             } else {
                 /*
-                 * Otherwise, this is the live file system.
+                 * Otherwise, if everything matches, this is the live file
+                 * system itself.
                  */
                 Some(WhatIsIt::Live(ent.special.to_string()))
             }
